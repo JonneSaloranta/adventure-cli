4df8f3a55a98b3c736bcdab4845da0b2d2d4623d
Jonne <saloranta.jonne@gmail.com>
2024-11-06 01:31:45 +0200

    test: added tests for world seed

--- test/world/test_world_gen.py
+++ test/world/test_world_gen.py
@@ -5,7 +5,7 @@ from src.world.world_gen import WorldGenerator
 class TestWorldGenerator(unittest.TestCase):
     
     def setUp(self):
-        self.world_gen = WorldGenerator("test")
+        self.world_gen = WorldGenerator(seed="test")
 
     def tearDown(self):
         # print("tearDown")
@@ -18,6 +18,12 @@ class TestWorldGenerator(unittest.TestCase):
         self.assertEqual(len(self.world_gen.generate_chunk(0, 0, 5)[0]), 5)
         self.assertEqual(len(self.world_gen.generate_chunk(0, 0, 1)), 1)
         self.assertEqual(len(self.world_gen.generate_chunk(0, 0, 1)[0]), 1)
+    
+    def test_generate_combined_seed(self):
+        self.assertEqual(self.world_gen.generate_combined_seed("test", 0, 0), "test_0_0")
+
+    def test_get_seed(self):
+        self.assertEqual(self.world_gen.get_seed(), "test")
 
 
 if __name__ == "__main__":
