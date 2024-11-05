b2d71c2caffe5f191eaba4c1de10df359770f2da
Jonne <saloranta.jonne@gmail.com>
2024-11-05 21:33:04 +0200

    chore: removed unused python file for running tests

--- run_tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-if __name__ == "__main__":
-    # Run all tests in the test directory
-    loader = unittest.TestLoader()
-    suite = loader.discover("test")
-    runner = unittest.TextTestRunner()
-
-    runner.run(suite)
