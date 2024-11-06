aec84bba22d29cd78b2b1bab4f8cd182adce9707
Jonne <saloranta.jonne@gmail.com>
2024-11-06 20:48:04 +0200

    chore: updated save and load functions

--- src/util/serialize.py
+++ src/util/serialize.py
@@ -1,16 +1,16 @@
 import pickle
+import os
 
 class Serializer:
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, default_path: str):
+        self.default_path = default_path
 
-    def save(self, obj):
-        try:
-            with open(self.path, 'wb') as f:
-                pickle.dump(obj, f)
-        except FileNotFoundError:
-            print("File not found")
+    def save(self, obj, path=None):
+        save_path = path or self.default_path
+        with open(save_path, 'wb') as f:
+            pickle.dump(obj, f)
 
-    def load(self):
-        with open(self.path, 'rb') as f:
+    def load(self, path=None):
+        load_path = path or self.default_path
+        with open(load_path, 'rb') as f:
             return pickle.load(f)
