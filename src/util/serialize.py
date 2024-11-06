acedded651e937070cb34ceee826caced0f98a23
JonneSaloranta <saloranta.jonne@gmail.com>
2024-11-06 22:11:28 +0200

    fix: ensure default path has .pkl file extension in Serializer

--- src/util/serialize.py
+++ src/util/serialize.py
@@ -1,9 +1,11 @@
 import pickle
 import os
-
 class Serializer:
     def __init__(self, default_path: str):
         self.default_path = default_path
+        fileformat = ".pkl"
+        if not self.default_path.endswith(fileformat):
+            self.default_path.join(fileformat)
 
     def save(self, obj, path=None):
         save_path = path or self.default_path
